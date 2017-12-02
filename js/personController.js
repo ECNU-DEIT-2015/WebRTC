66722f430384dcc6663761885bf64f10a971b9d2
Lethemay <10154507113@ecnu.cn>
2017-12-02 23:33:39 +0800

    boostrap & jquery

---
+++ js/personController.js
@@ -0,0 +1,8 @@
+angular.module('myApp', []).controller('personCtrl', function($scope) {
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+    $scope.fullName = function() {
+        return $scope.firstName + " " + $scope.lastName;
+    }
+});
+
