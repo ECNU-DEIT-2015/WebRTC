34c3ae763cb65e39087dc4037f625ed1f6150528
wqngze0 <10152510231@stu.ecnu.edu.cn>
2017-12-04 14:10:53 +0800

    初步完成视频功能

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
-var fs = require('fs');
-var url = require('url');
+'use strict';
+
+var os = require('os');
+var nodeStatic = require('node-static');
+// var http = require('http');
+// var socketIO = require('socket.io');
+
+// var fileServer = new(nodeStatic.Server)();
+// var app = http.createServer(function(req, res) {
+//     fileServer.serve(req, res);
+// }).listen(8080);
+
+// var express = require('express');
+// app.use(express.static('public'));
+// app.use("/", express.static(__dirname));
+// var bodyParser = require('body-parser');
+// app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.json());
+// var io = socketIO.listen(app);
+
+// var io = socketIO.listen(app_);
 
 var express = require('express');
 var app = express();
-app.use(express.static('public'));
+// // app.use(express.static('public'));
+app.use("/", express.static(__dirname));
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+// var io = socketIO.listen(app);
 
 var http = require('http').Server(app);
 var io = require("socket.io")(http);
@@ -15,12 +36,74 @@ app.get('/', function(req, res){
   res.sendFile(__dirname+"/index.html");
 });
 
-io.on("connection", function(socket){
+var connectCounter = 0;
+// numClients
+// io.sockets.on("connection", function(socket){
+  io.on("connection", function(socket){
+    connectCounter ++;
+    console.log(connectCounter);
     socket.on("array_changed", function(msg){
         console.log("array_changed");
         socket.broadcast.emit("array_changed", msg);
     });
-});
+
+    // convenience function to log server messages on the client
+  function log() {
+    var array = ['Message from server:'];
+    array.push.apply(array, arguments);
+    socket.emit('log', array);
+  }
+
+  socket.on('message', function(message) {
+    log('Client said: ', message);
+    // for a real app, would be room-only (not broadcast)
+    socket.broadcast.emit('message', message);
+  });
+
+
+  socket.on('create or join', function(room) {
+    log('Received request to create or join room ' + room);
+
+    // var numClients = io.sockets.sockets.length;
+    var numClients = io.sockets.length;
+    log('Room ' + room + ' now has ' + connectCounter + ' client(s)');
+
+    if (connectCounter === 1) {
+      socket.join(room);
+      log('Client ID ' + socket.id + ' created room ' + room);
+      socket.emit('created', room, socket.id);
+
+    } else if (connectCounter < 10) {
+      log('Client ID ' + socket.id + ' joined room ' + room);
+      io.sockets.in(room).emit('join', room);
+      socket.join(room);
+      socket.emit('joined', room, socket.id);
+      io.sockets.in(room).emit('ready');
+    } else { // max two clients
+      socket.emit('full', room);
+    }
+  });
+
+  socket.on('ipaddr', function() {
+    var ifaces = os.networkInterfaces();
+    for (var dev in ifaces) {
+      ifaces[dev].forEach(function(details) {
+        if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
+          socket.emit('ipaddr', details.address);
+        }
+      });
+    }
+  });
+
+  socket.on('bye', function(){
+    console.log('received bye');
+  });
+
+  socket.on('disconnect', function() {
+    connectCounter--;
+    console.log("disconnected",connectCounter);
+    });
+}); 
 
 http.listen(8080, function(){
     console.log('listening on *:8080');
---
+++ server_01.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var os = require('os');
+var nodeStatic = require('node-static');
+var http = require('http');
+var socketIO = require('socket.io');
+
+var fileServer = new(nodeStatic.Server)();
+var app = http.createServer(function(req, res) {
+  fileServer.serve(req, res);
+}).listen(8080);
