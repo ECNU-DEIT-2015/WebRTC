9a39bee1c7a69d64d4bd81ada4e1f9769d36e908
Lethemay <10154507113@ecnu.cn>
2017-12-18 23:32:22 +0800

    add_friend angular ng-include

--- js/my_angular.js
+++ js/my_angular.js
@@ -1,14 +1,26 @@
-var app = angular.module('shouye_app', []);
-app.controller('navigator_controller', function($scope, $http) {
-$scope.data_list = []
-$scope.$watch('search_value', function(newValue, oldValue) {
-        if (newValue === oldValue) {
-             return;
-        }else if((newValue.length==1 && oldValue==undefined) || (newValue.length > oldValue.length)){
-            $scope.data_list.push({"h":"老年","p":"架飞机阿咖酚散放辣椒发了卡机发","img":"../images/1.png"});
-        }else{
-            $scope.data_list.pop()
-        }
-        console.log("data changed");
-    }, true);
-});
+function load_angular(){
+    var app = angular.module('shouye_app', []);
+    app.controller('navigator_controller', function($scope, $http) {
+    $scope.data_list = []
+    $scope.$watch('search_value', function(newValue, oldValue) {
+            if (newValue === oldValue) {
+                 return;
+            }else if((newValue.length==1 && oldValue==undefined) || (newValue.length > oldValue.length)){
+                $scope.data_list.push({"h":"老年","p":"架飞机阿咖酚散放辣椒发了卡机发","img":"../images/1.png"});
+            }else{
+                $scope.data_list.pop()
+            }
+            console.log("data changed");
+        }, true);
+    });
+    
+    console.log("add_friend_constroller");
+    app.controller('add_friend_controller', function($http,$scope){
+        $scope.lasted_friend = [{'name':'wu','qianming':'be honest','profile':'../images/u_01.png'},
+            {'name':'qing','qianming':'be honest','profile':'../images/u_02.png'},
+            {'name':'ze','qianming':'be honest','profile':'../images/u_03.png'}];
+        $scope.search_friend = [];
+        console.log("add_friend_constroller");
+    });
+}
+
