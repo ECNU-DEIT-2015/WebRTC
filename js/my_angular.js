7e618828143564ba8ecb45b6caf09cdd5d050216
Lethemay <10154507113@ecnu.cn>
2017-12-22 03:45:03 +0800

    bin box

--- js/my_angular.js
+++ js/my_angular.js
@@ -298,20 +298,20 @@ function load_angular(){
     });
 
     app.controller('bin_controller', function($http,$scope){
-        $scope.lasted_bin_file = [{"h":"老年","p":"架飞机阿咖酚散放辣椒发了卡机发","img":"../images/1.png"},
-        {"h":"老年","p":"架飞机阿咖酚散放辣椒发了卡机发","img":"../images/2.png"},
-        {"h":"老年","p":"架飞机阿咖酚散放辣椒发了卡机发","img":"../images/3.png"}];
-
-        $scope.search_file = [];
-        $scope.$watch('search_value', function(newValue, oldValue) {
+        $scope.labels = [1,2,3,4];
+        $scope.search_file = []
+        $scope.lasted_file = [{"headline":"老年","introduction":"架飞机阿咖酚散放辣椒发了卡机发","image":"../images/1.png"},
+        {"headline":"老年","introduction":"架飞机阿咖酚散放辣椒发了卡机发","image":"../images/2.png"},
+        {"headline":"老年","introduction":"架飞机阿咖酚散放辣椒发了卡机发","image":"../images/3.png"}];
+        $scope.$watch('search_value', function(newValue,oldValue){
             if (newValue === oldValue) {
                 return;
-            }else if((newValue.length==1 && oldValue==undefined) || (newValue.length > oldValue.length)){
-                $scope.search_file.push({"h":"老年","p":"架飞机阿咖酚散放辣椒发了卡机发","img":"../images/1.png"});
-            }else{
-                $scope.search_file.pop()
-            }
-        }, true);
+           }else if((newValue.length==1 && oldValue==undefined) || (newValue.length > oldValue.length)){
+               $scope.search_file.push({"headline":"老年","introduction":"架飞机阿咖酚散放辣椒发了卡机发","image":"../images/3.png"});
+           }else{
+               $scope.search_file.pop()
+           }
+        },true);
     });
 
     app.controller('old_file_controller', function($http, $scope){
