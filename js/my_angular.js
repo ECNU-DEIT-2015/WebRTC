a241eed715748032ecfbc4533a651ee07ed13274
Lethemay <10154507113@ecnu.cn>
2017-12-22 04:20:07 +0800

    bin box

--- js/my_angular.js
+++ js/my_angular.js
@@ -362,7 +362,23 @@ function load_angular(){
         },true);
     });
 
-
+    app.controller('friend_controller', function($http, $scope){
+        $scope.lasted_friend = [{'name':'wu','qianming':'be honest','profile':'../images/u_01.png'},
+            {'name':'qing','qianming':'be honest','profile':'../images/u_02.png'},
+            {'name':'ze','qianming':'be honest','profile':'../images/u_03.png'}];
+       
+        $scope.search_friend = [];
+        console.log("add_friend_constroller");
+        $scope.$watch('search_value', function(newValue,oldValue){
+            if (newValue === oldValue) {
+                return;
+           }else if((newValue.length==1 && oldValue==undefined) || (newValue.length > oldValue.length)){
+               $scope.search_friend.push({'name':'ze','qianming':'be honest','profile':'../images/u_03.png'});
+           }else{
+               $scope.search_friend.pop()
+           }
+        },true);
+    });
     //测试filebox controller
     app.controller("file_box_controller", function($http,$scope){
         $scope.labels = [1,2,3,4];
